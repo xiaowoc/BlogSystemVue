d2b1290766a547ad559668463d52892e30bc2a5e
吴创业 <501710163@qq.com>
2020-03-10 00:12:39 +0800

    1、导航栏增加判断显示

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -4,15 +4,16 @@
       <el-menu-item index="/">首页</el-menu-item>
       <el-menu-item v-if="getUserId!=''" :index="'/UserDetails/'+ getUserId">个人主页</el-menu-item>
       <el-menu-item index="/CreateCategory">添加分类</el-menu-item>
-      <el-menu-item index="/Register">注册</el-menu-item>
-      <el-menu-item index="/Login">登陆</el-menu-item>
-      <el-menu-item index="/Logout">注销</el-menu-item>
+      <el-menu-item index="/CreateArticle">创建文章</el-menu-item>
       <el-menu-item index="/Search">搜索</el-menu-item>
+      <el-menu-item v-if="getUserId==''" index="/Register">注册</el-menu-item>
+      <el-menu-item v-if="getUserId==''" index="/Login">登陆</el-menu-item>
+      <el-menu-item v-if="getUserId!=''" index="/Logout">注销</el-menu-item>
       <!-- <el-menu-item index="/ForgetPassword">忘记密码</el-menu-item>
       <el-menu-item index="/ResetPassword">重置密码</el-menu-item>-->
       <!-- <el-menu-item index="/CategoryList/">分类列表</el-menu-item>
       <el-menu-item index="/ArticleList/">文章列表</el-menu-item>-->
-      <el-menu-item index="/CreateArticle">创建文章</el-menu-item>
+
       <!-- <el-menu-item index="/EditArticle">编辑文章</el-menu-item> -->
     </el-menu>
   </div>
